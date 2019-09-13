62aedd92927ce42a38040a738661e7094d0753ef
warmlyice <warmlyice@gmail.com>
2019-09-13 16:35:07 +0800

    fgs: io keyboard

--- c/fragments/io/test.c
+++ c/fragments/io/test.c
@@ -21,3 +21,18 @@ int main(int argc, char const *argv[])
   sleep(1);
   return 0;
 }
+
+//clang -framework ApplicationServices -framework Carbon main.c
+
+// https://stackoverflow.com/questions/40546333/how-to-simulate-typing-keypresses-in-c-on-mac-os
+// https://developer.apple.com/documentation/coregraphics/1456527-cgeventpost?language=objc
+   // https://stackoverflow.com/questions/10179995/how-to-simulate-a-unicode-char-key-press-in-mac-os-x-using-objective-c
+   /*
+   CGEventRef downEvt = CGEventCreateKeyboardEvent( NULL, 0, true );
+   CGEventRef upEvt = CGEventCreateKeyboardEvent( NULL, 0, false );
+   UniChar oneChar = 'h';
+   CGEventKeyboardSetUnicodeString( downEvt, 1, &oneChar );
+   CGEventKeyboardSetUnicodeString( upEvt, 1, &oneChar );
+   CGEventPost( kCGAnnotatedSessionEventTap, downEvt );
+   CGEventPost( kCGAnnotatedSessionEventTap, upEvt );
+    */
